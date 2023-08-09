67cf172e6ae50680a4766dd9d1305a598545035e
Prajwal <prajwalgbdr03@gmail.com>
2023-08-09 23:39:25 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,1 +1,29 @@
-from flask import Flask, 
+from flask import Flask, render_template, request, jsonify
+import os
+import yaml
+import joblib
+import pandas as pd
+import numpy as np
+from application_logging import logging
+
+
+
+
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def home():
+    return "hello world"
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    app.run()
